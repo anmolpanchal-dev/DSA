3b5d199fd2b316061c2a83a608f516366559151f
Anmol Panchal <panchalji9705@gmail.com>
2026-07-01 10:50:52 +0530

    Pairs in STl

---
+++ LearningCpp/STL/Pairs/program1.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // pair<string,int> p = {"Anmol", 19};
+    // cout<<p.first<<" Your age is "<<p.second<<endl;
+    // pair<int , pair<int, int>> p2 = {1,{2,3}};
+    // cout<<p2.first<<endl;
+    // cout<<p2.second.first<<endl;
+    // cout<<p2.second.second<<endl;int
+    pair<int,int> arr[] = {{1,2},{3,4},{5,6}};
+    cout<<arr[5].second;
+}
