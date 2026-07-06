b58e140f32ea6e313247338745faa0fd6fc3c81d
Anmol Panchal <panchalji9705@gmail.com>
2026-07-06 22:50:25 +0530

    Two Pointer

---
+++ two-pointer/tripletSumToZero.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> Something(int arr[],int arrSize){
+    vector<vector<int>> v1;
+    for(int i = 0; i < arrSize-2; i++){
+        int left = i+1;
+        int right = arrSize-1;
+        while(left<right){
+            int sum = arr[left]+arr[right];
+            if(sum == -arr[i]){
+                v1.push_back({arr[i],arr[left],arr[right]});
+                left++;
+                right--;
+                while(arr[left]==arr[left-1]){
+                    left++;
+                }
+                while(arr[right]==arr[right+1]){
+                    right--;
+                }
+            }else if(sum < 0){
+                left++;
+            }else{
+                right--;
+            }
+        }
+    }
+    return v1;
+}
+
+int main(){
+    int arr[] = {-4, -2, -2, -1, 0, 1, 2, 2, 3, 4};
+    int arrSize = sizeof(arr) / sizeof(arr[0]);
+
+    vector<vector<int>> result = Something(arr, arrSize);
+    for(auto pair : result){
+        cout<<"[ ";
+        for(auto triplet : pair){
+            cout<<triplet<<" ";
+        }
+        cout<<"]\n";
+    }
+    return 0;
+}
