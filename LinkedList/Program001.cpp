207ce175717e0bd5447a34250a4e0f89f44e73d4
Anmol Panchal <panchalji9705@gmail.com>
2026-07-12 15:47:59 +0530

    linked list insertion

--- LinkedList/Program001.cpp
+++ LinkedList/Program001.cpp
@@ -16,27 +16,40 @@ Node* insertAtBegining(Node* head , int value){
     head = element;
     return head;
 }
+Node* insertAtEnd(Node* head , int value){
+    Node* newNode = new Node(value);
+    if(head == nullptr){
+        return newNode;
+    }
+    Node* temp = head;
+    while(temp->next != nullptr){
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return head;
+}
 void printList(Node* head){
     while(head!=nullptr){
         cout<<head->data<<" -> ";
         head = head->next;
     }
-    cout<<  "NULL";
+    cout<<  "NULL"<<endl<<endl;
 }
 int main(){
     Node* first = new Node(134);
     Node* head = first;
-    head = insertAtBegining(head, 100);
-    head = insertAtBegining(head, 1000);
-    head = insertAtBegining(head, 10000);
-    head = insertAtBegining(head, 100000);
-    head = insertAtBegining(head, 10234);
-    head = insertAtBegining(head, 13240);
-    head = insertAtBegining(head, 10234);
-    head = insertAtBegining(head, 10);
-    head = insertAtBegining(head, 1042);
-    head = insertAtBegining(head, 12430);
-    head = insertAtBegining(head, 10432);
+    head = insertAtBegining(head, 1);
+    head = insertAtBegining(head, 2);
+    head = insertAtBegining(head, 3);
+    head = insertAtBegining(head, 4);
     printList(head);
+    cout<<"------------end insert----------------"<<endl<<endl;
+    head = insertAtEnd(head, 100);
+    head = insertAtEnd(head, 200);
+    head = insertAtEnd(head, 300);
+    head = insertAtEnd(head, 400);
+    printList(head);
+
+
     return 0;
 }
