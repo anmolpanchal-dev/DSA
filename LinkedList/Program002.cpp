0ef4a88211b8c50d57cd84f1f43c66b98cd0442d
Anmol Panchal <panchalji9705@gmail.com>
2026-07-21 07:51:44 +0530

    delete operation

--- LinkedList/Program002.cpp
+++ LinkedList/Program002.cpp
@@ -56,6 +56,47 @@ Node* insertAtPosition(Node* head, int pos, int val){
     return head;
 }
 
+Node* deleteAtBeginning(Node* head){
+    if(head == nullptr) retrun nullptr;
+    Node* temp = head;
+    temp -> next = head;
+    delete temp;
+    return head;
+
+}
+Node* deleteAtEnd(Node* head){
+    if(head == nullptr) retrun nullptr;
+    if(head-> == nullptr){
+        delete head;
+        retrun nullptr
+    }
+    Node* temp = head;
+    while(temp -> next -> next != nullptr){
+        temp = temp -> next;
+    }
+    delete temp -> next;
+    temp->next = nullptr;
+    return head;
+}
+Node* deleteAtPos(node* head, int pos){
+    if(pos < 0) retrun head ;
+    Node* temp = head;
+    if(pos == 0){
+        temp -> next = head;
+        delete temp;
+        return head;
+    }
+    int count = 0;
+    while(temp != nullptr && count < pos-1){
+        temp = temp -> next;
+        count++;
+    }
+    if(temp == nullptr || temp->next = nullptr) return head;
+    Node* nodeToDelete = temp -> next;
+    temp->next = nodeToDelete -> next;
+    delete nodeToDelete;
+    return head;
+}
 
 int main(){
 
