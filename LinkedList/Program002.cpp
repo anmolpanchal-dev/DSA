bf01838b4536ce6f20ff7112bd0bcb6c5be84a5a
Anmol Panchal <panchalji9705@gmail.com>
2026-07-21 08:01:02 +0530

    ass and delete operation

--- LinkedList/Program002.cpp
+++ LinkedList/Program002.cpp
@@ -1,103 +1,128 @@
-#include<bits/stdc++.h>
-using namesapce std;
-struct Node{
+#include <iostream>
+using namespace std;
+
+struct Node {
     int data;
     Node* next;
-    Node(int val){
+    Node(int val) {
         data = val;
         next = nullptr;
-    } 
-}
+    }
+};
 
-Node* insertAtBegining(Node* head, int value){
+Node* insertAtBegining(Node* head, int value) {
     Node* element = new Node(value);
-    element -> next = head;
-    head = element;
-    return head;
+    element->next = head;
+    return element;
 }
 
-
-Node* insertEnd(Node* head, int value){
-    Node*element = new Node(value);
-    if(head == nullptr){
+Node* insertEnd(Node* head, int value) {
+    Node* element = new Node(value);
+    if (head == nullptr) {
         return element;
     }
     Node* temp = head;
-    while(temp->next != nullptr){
+    while (temp->next != nullptr) {
         temp = temp->next;
     }
     temp->next = element;
-    retrun head
+    return head;
 }
 
-void proint(Node* head){
-    while(head == nullptr){
-        cout<<head->data<<" -> ";
-        head = head->next;
-    }
-}
-
-Node* insertAtPosition(Node* head, int pos, int val){
-    if(pos < 0) return head;
-    Node* newNode = new Node(value);
-    if(pos == 0){
-        newNode -> next = head;
-        head = newNode
-        return head;
+Node* insertAtPosition(Node* head, int pos, int val) {
+    if (pos < 0) return head;
+    if (pos == 0) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        return newNode;
     }
     Node* temp = head;
     int count = 0;
-    while(temp != nullptr && count < pos-1){
+    while (temp != nullptr && count < pos - 1) {
         temp = temp->next;
-        count++
+        count++;
     }
-    newNode -> next = temp ->next;
+    if (temp == nullptr) return head;
+    Node* newNode = new Node(val);
+    newNode->next = temp->next;
     temp->next = newNode;
     return head;
 }
 
-Node* deleteAtBeginning(Node* head){
-    if(head == nullptr) retrun nullptr;
+Node* deleteAtBeginning(Node* head) {
+    if (head == nullptr) return nullptr;
     Node* temp = head;
-    temp -> next = head;
+    head = head->next;
     delete temp;
     return head;
-
 }
-Node* deleteAtEnd(Node* head){
-    if(head == nullptr) retrun nullptr;
-    if(head-> == nullptr){
+
+Node* deleteAtEnd(Node* head) {
+    if (head == nullptr) return nullptr;
+    if (head->next == nullptr) {
         delete head;
-        retrun nullptr
+        return nullptr;
     }
     Node* temp = head;
-    while(temp -> next -> next != nullptr){
-        temp = temp -> next;
+    while (temp->next->next != nullptr) {
+        temp = temp->next;
     }
-    delete temp -> next;
+    delete temp->next;
     temp->next = nullptr;
     return head;
 }
-Node* deleteAtPos(node* head, int pos){
-    if(pos < 0) retrun head ;
-    Node* temp = head;
-    if(pos == 0){
-        temp -> next = head;
+
+Node* deleteAtPos(Node* head, int pos) {
+    if (head == nullptr || pos < 0) return head;
+    if (pos == 0) {
+        Node* temp = head;
+        head = head->next;
         delete temp;
         return head;
     }
+    Node* temp = head;
     int count = 0;
-    while(temp != nullptr && count < pos-1){
-        temp = temp -> next;
+    while (temp != nullptr && count < pos - 1) {
+        temp = temp->next;
         count++;
     }
-    if(temp == nullptr || temp->next = nullptr) return head;
-    Node* nodeToDelete = temp -> next;
-    temp->next = nodeToDelete -> next;
+    if (temp == nullptr || temp->next == nullptr) return head;
+    Node* nodeToDelete = temp->next;
+    temp->next = nodeToDelete->next;
     delete nodeToDelete;
     return head;
 }
 
-int main(){
+void print(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " -> ";
+        head = head->next;
+    }
+    cout << "NULL\n";
+}
 
+int main() {
+    Node* head = nullptr;
+
+    head = insertEnd(head, 10);
+    head = insertEnd(head, 20);
+    head = insertEnd(head, 30);
+    print(head);
+
+    head = insertAtBegining(head, 5);
+    print(head);
+
+    head = insertAtPosition(head, 2, 15);
+    print(head);
+
+    head = deleteAtBeginning(head);
+    print(head);
+
+    head = deleteAtEnd(head);
+    print(head);
+
+    head = deleteAtPos(head, 1);
+    print(head);
+
+    return 0;
 }
